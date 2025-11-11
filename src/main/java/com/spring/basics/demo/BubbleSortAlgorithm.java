676be91e1626bd5ff996e6f2d1753ac1a9f018bf
thanujsai <sboda@modeln.com>
2025-11-11 14:33:32 +0530

    Primary annotation to let spring understand which bean to pick as a default one

--- src/main/java/com/spring/basics/demo/BubbleSortAlgorithm.java
+++ src/main/java/com/spring/basics/demo/BubbleSortAlgorithm.java
@@ -2,6 +2,7 @@ package com.spring.basics.demo;
 
 import org.springframework.stereotype.Component;
 
+//spring will be able to find this class because of the @Component annotation
 @Component//by declaring this we are asking spring to manage this class, spring creates a bean for it
 public class BubbleSortAlgorithm implements SortAlgorithm{
   public int[] sort(int[] numbers){
